f2ae76973460b143da5b424eb59cde0afd666577
bashmann <35540098+bashmann@users.noreply.github.com>
2026-05-16 13:45:44 +0100

    Remove obsolete PHP and HTML includes

--- index.php
+++
@@ -1,1 +0,0 @@
-<?php header("Location: /public/Home/home.php"); exit; ?>
--- template.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<?php 
-$root = $_SERVER['DOCUMENT_ROOT'];
-$title = "Technical Artist";
-include_once($root . "/Content/header.php")?>
-<body>
-    <!-- content -->
-</body>
